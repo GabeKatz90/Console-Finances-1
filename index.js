457188d430d13eac5f4093d74fd50fe752676c42
Isabelle Zennah <izennah@griffithsandarmour.com>
2022-11-11 14:08:29 +0000

    Added the variables in JS and a title in HTML 🦑

--- index.js
+++ index.js
@@ -86,3 +86,12 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+console.log("Financial Analysis \n----------------------------");
+
+// Variables
+var totalMonths;
+var totalNet;
+var average;
+var increase;
+var decrease;
